f1a8415d3f90e92f76fcaf86f55f0532721a5e2c
LucasGLaPro <70692658+LucasGLaPro@users.noreply.github.com>
2021-04-26 16:19:24 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,25 +1,47 @@
 #include <SFML/Graphics.hpp>
-#include "constante.h"
+#include <iostream>
 
 
-
-
-int main(int argc, char **argv)
+int main()
 {
-	sf::Window window(sf::VideoMode(800, 600), "My window");
 
-	// on fait tourner le programme jusqu'à ce que la fenêtre soit fermée
-	while (window.isOpen())
-	{
-		// on inspecte tous les évènements de la fenêtre qui ont été émis depuis la précédente itération
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			// évènement "fermeture demandée" : on ferme la fenêtre
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
-	}
+    sf::RenderWindow window(sf::VideoMode(1000, 500), "SFML ");
 
-	return 0;
+    sf::Image image;
+    if (!(image.loadFromFile("mario3.png")))
+        std::cout << "Cannot load image";   //Load Image
+
+    sf::Texture texture;
+    texture.loadFromImage(image);  //Load Texture from image
+
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
+
+
+    while (window.isOpen())
+    {
+
+        sf::Event event;
+
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+
+        }//Event handling done
+
+
+        for (int i = 0; i < 1280; i++)
+        {
+            for (int j = 0; j < 720; j++)
+                image.setPixel(i, j, sf::Color::Black);
+        }
+
+        window.clear();
+        window.draw(sprite);
+        window.display();
+    }
+
+
+    return 0;
 }
