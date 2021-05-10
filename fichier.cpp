2b888d653d15bbeb43e0612125d2d3029a53883b
Gregory-febvin <70692941+Gregory-febvin@users.noreply.github.com>
2021-05-10 17:16:19 +0200

    Add files via upload

--- fichier.cpp
+++ fichier.cpp
@@ -1,53 +1,53 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <SDL/SDL.h>
-#include <SDL/SDL_image.h>
 
-
+#pragma warning(disable : 4996)
 #include "constante.h"
-#include "fichiers.h"
+#include "fichier.h"
 
 
 int LoadNivau(int niveau[][NB_BLOCS_HAUTEUR])
 {
     FILE* fichier = NULL;
-    char lignefichier[NB_BLOCS_LARGEUR * NB_BLOCS_HAUTEUR + 1] = { 0 };
+	char ligneFichier[NB_BLOCS_LARGEUR * NB_BLOCS_HAUTEUR + 1] = { 0 };
     int i = 0, j = 0;
 
     fichier = fopen("niveaux.lvl", "r");
-    if (fichier == 0)
-        return 0;
+	if (fichier == 0) {
+		return 0;
+	} else {
+	
+		fgets(ligneFichier, NB_BLOCS_LARGEUR * NB_BLOCS_HAUTEUR + 1, fichier);
 
-    fgets(lignefichier, NB_BLOCS_LARGEUR * NB_BLOCS_HAUTEUR + 1, fichier);
+		for (i = 0; i < NB_BLOCS_LARGEUR; i++)
+		{
+			for (j = 0; j < NB_BLOCS_HAUTEUR; j++)
+			{
+				switch (ligneFichier[(i * NB_BLOCS_LARGEUR) + j])
+				{
+				case '0':
+					niveau[j][i] = 0;
+					break;
+				case '1':
+					niveau[j][i] = 1;
+					break;
+				case '2':
+					niveau[j][i] = 2;
+					break;
+				case '3':
+					niveau[j][i] = 3;
+					break;
+				case '4':
+					niveau[j][i] = 4;
+					break;
+				}
+			}
 
-    for (i = 0; i < NB_BLOCS_LARGEUR; i++)
-    {
-        for (j = 0; j < NB_BLOCS_HAUTEUR; j++)
-        {
-            switch (ligneFichier[(i * NB_BLOCS_LARGEUR) + j])
-            {
-            case '0':
-                niveau[j][i] = 0;
-                break;
-            case '1':
-                niveau[j][i] = 1;
-                break;
-            case '2':
-                niveau[j][i] = 2;
-                break;
-            case '3':
-                niveau[j][i] = 3;
-                break;
-            case '4':
-                niveau[j][i] = 4;
-                break;
-            }
-        }
 
+
+		}
+	}
         
-
-    }
-
     fclose(fichier);
     return 1;
 
--- jeu.cpp
+++ jeu.cpp
@@ -1,88 +1,100 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <SDL/SDL.h>
-#include <SDL/SDL_image.h>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <iostream>
 
-#include "constantes.h"
+#include "constante.h"
 #include "jeu.h"
+#include "fichier.h"
 
+using namespace std;
 
 void jouer(sf::RenderWindow *window)
 {
-    sf::RenderWindow window{ sf::VideoMode(800, 600), "Ecran" };
 
     sf::Sprite* mario[4] = { NULL };
-    sf::Sprite* mur = NULL, * caisse = NULL, * caisseOK = NULL, * objectif = NULL, * marioActuel = NULL;
-    SDL_Rect position, positionJoueur;
-    sf::Event event;
+	sf::Sprite  mur, caisse, caisseOk, objectif;
+	sf::Sprite* AllSprite[6] = {&mur, &caisse, &caisseOk, &objectif };
+    sf::Vector2i Position, PositionJoueur;
+    
+
+
 
     int niveau[NB_BLOCS_LARGEUR][NB_BLOCS_HAUTEUR] = { 0 };
 
     // Je charge toute les image possible du niveau
-    sf::Texture mur;
-    if (!texture.loadFromFile("mur.jpg"))
+    sf::Texture murTexture;
+    if (!murTexture.loadFromFile("mur.jpg"))
     {
         cout << "Mur non chargé";
     }
 
-    sf::Texture caisse;
-    if (!texture.loadFromFile("caisse.jpg"))
+    sf::Texture caisseTexture;
+    if (!caisseTexture.loadFromFile("caisse.jpg"))
     {
         cout << "Caisse non chargé";
     }
 
-    sf::Texture objectif;
-    if (!texture.loadFromFile("objectif.png"))
+    sf::Texture objectifTexture;
+    if (!objectifTexture.loadFromFile("objectif.png"))
     {
         cout << "Objectif non chargé";
     }
 
-    sf::Texture caisse_ok;
-    if (!texture.loadFromFile("caisse_ok.jpg"))
+    sf::Texture caisse_okTexture;
+    if (!caisse_okTexture.loadFromFile("caisse_ok.jpg"))
     {
         cout << "Caisse_OK non chargé";
     }
-    sf::Texture mario_droite;
-    if (!texture.loadFromFile("mario_droite.gif"))
+    sf::Texture mario_droiteTexture;
+    if (!mario_droiteTexture.loadFromFile("mario_droite.gif"))
     {
         cout << "Mario de droite non chargé";
     }
-    sf::Texture mario_gauche;
-    if (!texture.loadFromFile("mario_gauche.gif"))
+    sf::Texture mario_gaucheTexture;
+    if (!mario_gaucheTexture.loadFromFile("mario_gauche.gif"))
     {
         cout << "Mario de gauche non chargé";
     }
-    sf::Texture mario_haut;
-    if (!texture.loadFromFile("mario_haut.gif"))
+    sf::Texture mario_hautTexture;
+    if (!mario_hautTexture.loadFromFile("mario_haut.gif"))
     {
         cout << "Mario de haut non chargé";
     }
-    sf::Texture mario_bas;
-    if (!texture.loadFromFile("mario_bas.gif"))
+    sf::Texture mario_basTexture;
+    if (!mario_basTexture.loadFromFile("mario_bas.gif"))
     {
         cout << "Mario de bas non chargé";
     }
 
-    marioDefault = mario[BAS]; // Je charge un Mario vers le bas par défaut
+    //marioDefault = mario[BAS]; // Je charge un Mario vers le bas par défaut
 
 
-    if (!chargerNiveau(niveau))// Je tente de charger le niveaux et si il échoue alors on ferme tout 
+    if (!LoadNivau(niveau))// Je tente de charger le niveaux et si il échoue alors on ferme tout 
         exit(EXIT_FAILURE);
 
 
-    for (i = 0; i < NB_BLOCS_LARGEUR; i++) //on cherche la position de mario sur la map
+    for (int i = 0; i < NB_BLOCS_LARGEUR; i++) //on cherche la position de mario sur la map
     {
-        for (j = 0; j < NB_BLOCS_HAUTEUR; j++)
+        for (int j = 0; j < NB_BLOCS_HAUTEUR; j++)
         {
-            if (niveau[i][j] == mario)
+            if (i == Position.x && j == Position.y)
             {
-                positionJoueur.x = i;
-                positionJoueur.y = j;
+                PositionJoueur.x = i;
+                PositionJoueur.y = j;
                 niveau[i][j] = VIDE;
             }
         }
     }
+
+		
+		for (int largeur = 0; largeur < NB_BLOCS_LARGEUR; largeur++) {
+			for (int hauteur = 0; hauteur < NB_BLOCS_HAUTEUR; hauteur++) {
+				sf::Sprite* asset = AllSprite[niveau[largeur][hauteur]];
+				window->draw(*asset);
+			}
+		}
+		window->display();
 }
+
